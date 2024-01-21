44abe342fe8f7a8c76fa9ba98b91e68c1a9aeee1
Steeven Salgado <salgadosteeven@gmail.com>
2024-01-21 21:20:26 +0100

    correction projet

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,29 @@
-let divParent = document.querySelector(".container")
+let divParent = document.querySelector(".container");
 for (let index = 0; index < 340; index++) {
-   let divCreer = document.createElement("div")
-   divCreer.className = "divEnfant" 
-    divParent.appendChild(divCreer)
-   divCreer.style.background = "#3a3a3a"
-    divCreer.style.width = "23px"
-    divCreer.style.height = "26px"
-    divCreer.style.margin = "1px"
+  //! La boucle qui permet de créer les div
+  let divCreer = document.createElement("div");
+  divCreer.className = "divEnfant"; //! Ajout de la class au div
+  divParent.appendChild(divCreer);
+  divCreer.style.background = "#3a3a3a"; //! Ajout de style aux div
+  divCreer.style.width = "23px";
+  divCreer.style.height = "26px";
+  divCreer.style.margin = "1px";
 }
 
-let lesDivEnfant = document.querySelectorAll(".divEnfant")
-console.log(lesDivEnfant);
+let lesDivEnfant = document.querySelectorAll(".divEnfant");
 
 function randomColor() {
-    return choose = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return (choose = "#" + Math.floor(Math.random() * 16777215).toString(16)); //! Calcul pour une couleur aléatoire
 }
 
-lesDivEnfant.forEach(element => {
-    element.addEventListener("mouseover", () => {
-        element.style.background = randomColor()
-    })
-    element.addEventListener("mouseout", () => {
-        element.style.background = "#3a3a3a"
-        element.style.transition = "3s"
-    })
+lesDivEnfant.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    element.style.background = randomColor();
+    element.style.borderRadius = "50%"
+  });
+  element.addEventListener("mouseout", () => {
+    element.style.background = "#3a3a3a";
+    element.style.transition = "4s ease";
+    element.style.borderRadius = "0"
+  });
 });
